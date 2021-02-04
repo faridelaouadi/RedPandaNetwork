e0ba3ac53504fb934547d1eda0cd209135577b6d
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2021-02-04 17:17:08 +0000

    first commit

---
+++ app_config.py
@@ -0,0 +1,28 @@
+import os
+
+CLIENT_ID = "519684b0-f491-4de5-9b6b-d97b28213c6e" # Application (client) ID of app registration
+
+CLIENT_SECRET = "E1VVY.5DNf5aBekC5WX3N346x~b6F~4-_8" # Placeholder - for use ONLY during testing.
+# In a production app, we recommend you use a more secure method of storing your secret,
+# like Azure Key Vault. Or, use an environment variable as described in Flask's documentation:
+# https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
+# CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+# if not CLIENT_SECRET:
+#     raise ValueError("Need to define CLIENT_SECRET environment variable")
+
+AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
+# AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+
+REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
+                              # The absolute URL must match the redirect URI you set
+                              # in the app's registration in the Azure portal.
+
+# You can find more Microsoft Graph API endpoints from Graph Explorer
+# https://developer.microsoft.com/en-us/graph/graph-explorer
+ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
+
+# You can find the proper permission names from this document
+# https://docs.microsoft.com/en-us/graph/permissions-reference
+SCOPE = ["User.ReadBasic.All"]
+
+SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
---
+++ static/js/azureMap.js
@@ -0,0 +1,134 @@
+var map;
+var datasource, popup;
+
+function GetMap() {
+    //Initialize a map instance.
+    map = new atlas.Map('myMap', {
+    center: [85.331,27.72],
+    zoom: 8,
+    view: 'Auto',
+    style: 'night',
+
+        //Add authentication details for connecting to Azure Maps.
+        authOptions: {
+        authType: 'subscriptionKey',
+        subscriptionKey: 'Z6miLmU_fd30J0nkg5OuTkviQ6UmhCFCxDyMLMHeRCY'
+    }
+    });
+
+    //Wait until the map resources are ready.
+    map.events.add('ready', function () {
+
+      map.controls.add(new atlas.control.StyleControl({
+        mapStyles: ['road', 'grayscale_dark', 'night', 'road_shaded_relief', 'satellite', 'satellite_road_labels']
+      }), {
+        position: 'top-right'
+      });  
+        
+        //Create a data source and add it to the map.
+        camera_datasource = new atlas.source.DataSource(null, {
+            cluster: true
+        });
+
+        panda_datasource = new atlas.source.DataSource(null, {
+          cluster: true,
+
+          //The radius in pixels to cluster points together.
+          clusterRadius: 100
+        });
+
+        map.sources.add(camera_datasource);
+        map.sources.add(panda_datasource);
+
+        var panda_img = "https://raw.githubusercontent.com/faridelaouadi/LeetCode/master/red_panda-removebg-preview.png"
+        var camera_img = 'https://cdn.iconscout.com/icon/free/png-512/apple-camera-493147.png'
+        
+        map.imageSprite.add('camera_icon', camera_img)
+        map.imageSprite.add('panda_icon', panda_img)
+
+
+        //Create three point features on the map and add some metadata in the properties which we will want to display in a popup.
+        var camera1 = new atlas.data.Feature(new atlas.data.Point([85.331,27.72]), {modalLink:'#camera1', cameraID: '0010', category:"camera"});
+        var camera2 = new atlas.data.Feature(new atlas.data.Point([86.331,27.72]), {modalLink:'#camera1', cameraID: '0011', category:"camera"});
+        var camera3 = new atlas.data.Feature(new atlas.data.Point([85.331,28.12]), {modalLink:'#camera1', cameraID: '0012', category:"camera"});
+        var camera4 = new atlas.data.Feature(new atlas.data.Point([84.331,27.72]), {modalLink:'#camera1', cameraID: '0013', category:"camera"});
+
+        //pandas next to camera 1
+        var panda1 = new atlas.data.Feature(new atlas.data.Point([85.331,27.52]), {category:"panda"});
+        var panda2 = new atlas.data.Feature(new atlas.data.Point([85.431,27.32]), {category:"panda"});
+        var panda3 = new atlas.data.Feature(new atlas.data.Point([85.531,27.62]), {category:"panda"});
+        var panda4 = new atlas.data.Feature(new atlas.data.Point([85.431,27.72]), {category:"panda"});
+
+        //pandas next to camera 2
+        var panda5 = new atlas.data.Feature(new atlas.data.Point([86.331,27.22]), {category:"panda"});
+        var panda6 = new atlas.data.Feature(new atlas.data.Point([86.421,27.62]), {category:"panda"});
+        var panda7 = new atlas.data.Feature(new atlas.data.Point([86.561,27.32]), {category:"panda"});
+        var panda8 = new atlas.data.Feature(new atlas.data.Point([86.611,27.72]), {category:"panda"});
+
+        //pandas next to camera 3
+        var panda9 = new atlas.data.Feature(new atlas.data.Point([85.231,28.12]), {category:"panda"});
+        var panda10 = new atlas.data.Feature(new atlas.data.Point([85.421,28.12]), {category:"panda"});
+        var panda11 = new atlas.data.Feature(new atlas.data.Point([85.561,28.12]), {category:"panda"});
+        var panda12 = new atlas.data.Feature(new atlas.data.Point([85.611,28.12]), {category:"panda"});
+
+        //pandas next to camera 4
+        var panda9 = new atlas.data.Feature(new atlas.data.Point([84.391,27.72]), {category:"panda"});
+        var panda10 = new atlas.data.Feature(new atlas.data.Point([84.331,27.79]), {category:"panda"});
+        var panda11 = new atlas.data.Feature(new atlas.data.Point([84.271,27.72]), {category:"panda"});
+        var panda12 = new atlas.data.Feature(new atlas.data.Point([84.331,27.68]), {category:"panda"});
+
+        camera_datasource.add([camera1,camera2,camera3,camera4]);
+        panda_datasource.add([panda1,panda2,panda3,panda4,panda5,panda6,panda7,panda8,panda9,panda10,panda11,panda12]);
+
+        //Basically adding the image to the points
+        var cameraSymbolLayer = new atlas.layer.SymbolLayer(camera_datasource, null, {
+            iconOptions: {
+            image: 'camera_icon',
+            size: 0.1
+            },
+            textOptions: {
+                    //Convert the temperature property of each feature into a string and concatenate "°F".
+                    textField: ['concat',  "Camera ID :", ['to-string', ['get', 'cameraID']]],
+                    font: [
+                        "SegoeFrutigerHelveticaMYingHei-Bold"
+                      ],
+
+                    //Offset the text so that it appears on top of the icon.
+                    offset: [0, -3.9]
+                }
+        });
+
+        var pandaSymbolLayer = new atlas.layer.SymbolLayer(panda_datasource, null, {
+            iconOptions: {
+            image: 'panda_icon',
+            size: 0.2
+            },
+        });
+
+        map.layers.add(new atlas.layer.HeatMapLayer(panda_datasource, null, {
+          //Set the weight to the point_count property of the data points.
+          weight: ['get', 'point_count'],
+
+          //Optionally adjust the radius of each heat point.
+          radius: 40
+      }), 'labels');
+
+        
+        map.layers.add(pandaSymbolLayer);
+        map.layers.add(cameraSymbolLayer);
+
+        //Add a click event to the symbol layer.
+        map.events.add('click', cameraSymbolLayer, function (e) {
+            //Get custom properties on the marker
+            var properties = e.shapes[0].getProperties();
+            $(properties.modalLink).modal('show')
+            
+        });
+        });
+    }
+
+
+
+
+
+window.addEventListener("load", GetMap);
